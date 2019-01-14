c1c4b8a06b143e7de29a161a2f2ccdcca78d75d5
deepak <marnenideepak4@gmail.com>
2019-01-14 21:37:22 +0530

    initial commit

---
+++ src/main/java/com/infosys/infytel/customer/zipkin/InfytelZipkinApplication.java
@@ -0,0 +1,14 @@
+package com.infosys.infytel.customer.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfytelZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfytelZipkinApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/infosys/infytel/customer/zipkin/InfytelZipkinApplicationTests.java
@@ -0,0 +1,17 @@
+package com.infosys.infytel.customer.zipkin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfytelZipkinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
